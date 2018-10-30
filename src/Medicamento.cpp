f4e5824c188b5c2bf992c4112f849f14bb714c8d
luispcunha <luispplmcunha@gmail.com>
2018-10-30 12:46:15 +0000

    Traduzi as classes Medicamento, Produto e Venda

---
+++ src/Medicamento.cpp
@@ -0,0 +1,22 @@
+#include "Medicamento.h"
+
+Medicamento::Medicamento(long int cod, string n, string desc, float pr, bool vendSemRec, bool pdSerRec, float descRec): Produto(cod, n, desc, pr) {
+	vendidoSemRec = vendSemRec;
+	podeSerRec = pdSerRec;
+	descComReceita = descRec;
+}
+
+
+float Medicamento::descontoComReceita() const {
+	return descComReceita;
+}
+
+bool Medicamento::vendidoSemReceita() const {
+	return vendidoSemRec;
+}
+
+bool Medicamento::podeSerReceitado() const {
+	return podeSerRec;
+}
+
+
---
+++ src/Medicamento.h
@@ -0,0 +1,18 @@
+#ifndef MEDICAMENTO_H_
+#define MEDICAMENTO_H_
+
+#include "Produto.h"
+
+class Medicamento : public Produto {
+	Medicamento(long int cod, string n, string desc, float pr, bool vendSemRec, bool pdSerRec, float descRec);
+	float descontoComReceita() const;
+	bool podeSerReceitado() const;
+	bool vendidoSemReceita() const;
+private:
+	const bool vendidoSemRec;
+	const bool podeSerRec;
+	const float descComReceita;
+};
+
+
+#endif /* MEDICAMENTO_H_ */
