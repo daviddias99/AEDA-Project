c76a051731cda68ff96f7f49688a9aceadf386c0
Pasgas99 <pasgas99@hotmail.com>
2018-11-22 15:25:14 +0000

    Adicionei comentarios do doxygen

--- src/Medicamento.h
+++ src/Medicamento.h
@@ -5,11 +5,45 @@
 
 class Medicamento : public Produto {
 public:
+	/**
+	* @brief Construtor da classe Medicamento
+	* @param cod Codigo do medicamento
+	* @param n Nome do medicamento
+	* @param desc Descricao do medicamento
+	* @param pr Preco do medicamento
+	* @param i Iva do medicamento
+	* @param vendSemRec True se e possivel vender o medicamento sem receita
+	* @param pdSerRec True se o medicamento pode ser receitado
+	* @param descRec Valor do desconto no caso do medicamento ser receitado
+	*/
 	Medicamento(unsigned long int cod, string n, string desc, float pr, float i, bool vendSemRec, bool pdSerRec, float descRec);
+
+	/**
+	* @brief Devolve o valor do desconto do medicamento quando e receitado
+	* @param Valor do desconto quando e receitado
+	*/
 	float descontoComReceita() const;
+
+	/**
+	* @brief Indica se o medicamento pode ser receitado
+	* @return Retorna true se o medicamento pode ser receitado
+	*/
 	bool podeSerReceitado() const;
+
+	/**
+	* @brief Indica se o medicamento pode ser vendido sem receita
+	* @return Retorna true se o medicamento pode ser vendido sem receita
+	*/
 	bool vendidoSemReceita() const;
+
+	/**
+	* @brief Copia para um ficheiro indicado no seu argumento os atributos do objeto de forma a ser facilmente carregado
+	*/
 	ostream& printSimp(ostream& os) const;
+
+	/**
+	* @brief Mostra no ecra os atributos do medicamento
+	*/
 	ostream& print(ostream& os) const;
 private:
 	const bool vendidoSemRec;
--- src/Produto.h
+++ src/Produto.h
@@ -8,15 +8,64 @@ using namespace std;
 
 class Produto {
 public:
+	/**
+	* @brief Construtor da classe Produto. 
+	* @param code Codigo do produto
+	* @param n Nome do produto
+	* @param dec Descricao do produto
+	* @param pr Preco do produto
+	* @param i Iva do produto
+	*/
 	Produto(long int code, string n, string desc, float pr, float i);
+
+	/**
+	* @brief Devolve o codigo do produto
+	* @return Codigo do produto
+	*/
 	long unsigned int getCodigo() const;
+
+	/**
+	* @brief Devolve o nome do produto
+	* @return Nome do produto
+	*/
 	string getNome() const;
+
+	/**
+	* @brief Devolve a descricao do produto
+	* @return Descricao do produto
+	*/
 	string getDesc() const;
+
+	/**
+	* @brief Devolve o preco do produto
+	* @return Preco do produto
+	*/
 	float getPreco() const;
+
+	/**
+	* @brief Altera o preco do produto para o indicado no seu argumento
+	* @param novoPreco Novo preco do produto
+	* @return Retorna 0 se a alteracao tiver sucesso
+	* Retorna 1 se o argumento for um valor negativo
+	*/
 	int alteraPreco(float novoPreco);
+
+	/**
+	* @brief Compara a igualdade do proprio objeto com outro produto. Dois produtos sao iguais se tem o mesmo codigo
+	* @param prod Produto a ser comparado
+	* @return Retorna true se os produtos comparados tiverem o mesmo codigo. 
+	* Retorna false se isso nao se verificar
+	*/
 	bool operator==(const Produto &prod);
 	
+	/**
+	* @brief Mostra no ecra os atributos do objeto
+	*/
 	virtual ostream& print(ostream& os) const;
+
+	/**
+	* @brief Copia para um ficheiro indicado no seu argumento os atributos do objeto de forma a que essa informacao seja facilmente carregada
+	*/
 	virtual ostream& printSimp(ostream& os) const;
 protected:
 	const long unsigned int codigo;
